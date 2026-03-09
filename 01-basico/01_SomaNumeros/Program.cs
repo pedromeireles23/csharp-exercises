81780aa35d88142bb25af6d5b68e614bff7609a2
pedromeireles23 <pemeirels@gmail.com>
2026-03-09 14:33:56 -0300

    estruturação de pastas + primeiro exercício básico feito + configuração do gitignore

---
+++ 01-basico/01_SomaNumeros/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_SomaNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO Crie duas variáveis inteiras e mostre a soma.
+
+            int num1 = 2;
+            int num2 = 3;
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+        }
+    }
+}
