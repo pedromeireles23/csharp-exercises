5cf2a6ab568c3d49e535b42656093de73c2d0545
pedromeireles23 <pemeirels@gmail.com>
2026-03-09 20:02:00 -0300

    exercicio basico 02

---
+++ 01-basico/02_MediaNotas/Program.cs
@@ -0,0 +1,8 @@
+﻿//TODO Crie três notas double e calcule a média.
+
+double nota1 = 8.5;
+double nota2 = 7.0;
+double nota3 = 9.0;
+double media = (nota1 + nota2 + nota3) / 3.0;
+Console.WriteLine(media);
+Console.ReadLine();
