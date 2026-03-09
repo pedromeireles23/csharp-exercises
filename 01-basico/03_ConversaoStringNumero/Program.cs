cc408d195414d9baaca39e7031a8d557ed2592d5
pedromeireles23 <pemeirels@gmail.com>
2026-03-09 20:26:16 -0300

    exercício basico 03 conversão string numero

---
+++ 01-basico/03_ConversaoStringNumero/Program.cs
@@ -0,0 +1,16 @@
+﻿//TODO Leia um número como string e converta para inteiro.
+Console.WriteLine("Digite um número:");
+string str = Console.ReadLine();
+Console.WriteLine($"O número é string? {str is string}");
+int num = Convert.ToInt32(str);
+Console.WriteLine("Então converteremos..");
+Console.WriteLine($"O número é string? {num is string}");
+
+
+
+
+
+
+Console.ReadLine();
+
+
