a8b5b83bc54b4fa666f4790620a4a7ab9ff37e9b
pedromeireles23 <pemeirels@gmail.com>
2026-03-10 01:18:58 -0300

    Basico 07

---
+++ 01-basico/07_UsandoVar/Program.cs
@@ -0,0 +1,10 @@
+﻿// TODO 📝 Declare variáveis usando var.
+var nome = "Pedro";
+var dinheiro = 1000.50;
+var dia = 24;
+var pi  = 3.14159;
+Console.WriteLine(nome);
+Console.WriteLine(dinheiro);
+Console.WriteLine(dia);
+Console.WriteLine(pi);
+Console.ReadLine();
