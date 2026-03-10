6129a005bb23dcab924683c79ed6ae7b7fa6bc64
pedromeireles23 <pemeirels@gmail.com>
2026-03-10 01:30:32 -0300

    basico 08

---
+++ 01-basico/08_ConstantePI/Program.cs
@@ -0,0 +1,8 @@
+﻿// TODO 📝 Crie uma constante PI e calcule a área de um círculo.
+const double PI = 3.14;
+Console.WriteLine("Digite o raio do círculo:");
+double raio = double.Parse(Console.ReadLine());
+//double area = PI * (raio * raio);
+double area = Math.Round(Math.PI * Math.Pow(raio, 2),2);
+Console.WriteLine($"A área do círculo é: {area}");
+Console.ReadLine();
