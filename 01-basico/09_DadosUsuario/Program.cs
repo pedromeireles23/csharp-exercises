44d6307f9f9bd4568a48c92afb77a917eb857c52
pedromeireles23 <pemeirels@gmail.com>
2026-03-10 01:42:57 -0300

    basico 09

---
+++ 01-basico/09_DadosUsuario/Program.cs
@@ -0,0 +1,9 @@
+﻿// TODO 📝 Peça nome e idade ao usuário.
+
+Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine() ?? string.Empty;
+Console.WriteLine("Digite sua idade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Nome: {nome} ");
+Console.WriteLine($"Idade: {idade} ");
+Console.ReadLine();
