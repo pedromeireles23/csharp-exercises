ddb1dd0642d4205b805e24187710e672d7bb4bc0
pedromeireles23 <pemeirels@gmail.com>
2026-03-10 22:50:45 -0300

    condicionais 02

---
+++ 02-condicionais/02_MaiorNumero/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Comparação de números -----");
+Console.WriteLine("Entre com o primeiro número: ");
+int primeiroNumero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Entre com o segundo número");
+int segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+if (primeiroNumero > segundoNumero)
+{
+    Console.WriteLine($"O número maior é {primeiroNumero}");
+
+}
+else if (primeiroNumero < segundoNumero)
+{
+    Console.WriteLine($"O número maior é {segundoNumero}");
+}
+else
+{
+    Console.WriteLine("Os números são iguais");
+}
+
+    Console.ReadLine();
