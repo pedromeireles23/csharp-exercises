2713cc66457dd2bb0ca0662419ee3b737fbb6aab
pedromeireles23 <pemeirels@gmail.com>
2026-03-11 03:23:13 -0300

    condicionais 06

---
+++ 02-condicionais/06_OperadorTernario/Program.cs
@@ -0,0 +1,7 @@
+﻿// TODO 📝 Use operador ternário para verificar maior de idade.
+
+int idade = 20;
+
+string resultado = idade >= 18 ? "Maior de idade" : "Menor de idad";
+Console.WriteLine(resultado);
+Console.ReadLine();
