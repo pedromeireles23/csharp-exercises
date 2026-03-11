1db94eb79fc2a54751ecf0ac0237280c9d01ce8a
pedromeireles23 <pemeirels@gmail.com>
2026-03-10 23:23:05 -0300

    condicionais 04

---
+++ 02-condicionais/04_ClassificacaoNota/Program.cs
@@ -0,0 +1,22 @@
+﻿//TODO Classifique uma nota:
+//= 7 aprovado
+//= 5 recuperação
+//< 5 reprovado
+
+Console.WriteLine("Digite a nota: ");
+double nota = Convert.ToDouble(Console.ReadLine());
+
+if(nota >= 7)
+{
+    Console.WriteLine("Aprovado");
+}
+else if (nota >= 5)
+{
+    Console.WriteLine("Recuperação");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+}
+
+Console.ReadLine();
