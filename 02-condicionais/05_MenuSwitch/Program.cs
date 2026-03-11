e30a102470d0a3e52758df91c25ba9f70b8b8a5f
pedromeireles23 <pemeirels@gmail.com>
2026-03-11 03:30:36 -0300

    condicionais 05

--- 02-condicionais/05_MenuSwitch/Program.cs
+++ 02-condicionais/05_MenuSwitch/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//TODO 📝 Crie um menu usando switch.
+
+Console.WriteLine("1 - Cadastrar");
+Console.WriteLine("2 - Listar");
+Console.WriteLine("3 - Sair");
+int input = Convert.ToInt32(Console.ReadLine());
+
+switch (input)
+{
+    case 1:
+        Console.WriteLine("Cadastrar selecionado");
+        break;
+    case 2:
+        Console.WriteLine("Listando dados...");
+        break;
+    case 3:
+        Console.WriteLine("Saindo...");
+        break;
+    default:
+        Console.WriteLine("Opção inválida");
+        break;
+}
+
+Console.ReadLine();
