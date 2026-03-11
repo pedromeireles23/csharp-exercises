b1f73744cddb549e18b9909ffd6dfe5c4cc6c4cf
pedromeireles23 <pemeirels@gmail.com>
2026-03-10 23:03:56 -0300

    condicionais 03

---
+++ 02-condicionais/03_VerificarPar/Program.cs
@@ -0,0 +1,14 @@
+﻿// TODO 📝 Verifique se um número é par ou ímpar.
+Console.WriteLine("Entre com um número");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine("O número é par");
+}
+else
+{
+    Console.WriteLine("O número é ímpar");
+}
+
+Console.ReadLine();
