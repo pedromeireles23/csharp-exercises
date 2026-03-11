919c9d1115d885fe04501ac03edb91260931e38b
pedromeireles23 <pemeirels@gmail.com>
2026-03-10 22:22:09 -0300

    condicionais 01

---
+++ 02-condicionais/01_NumeroPositivoNegativo/Program.cs
@@ -0,0 +1,18 @@
+﻿// TODO 📝 Verifique se um número é positivo ou negativo.
+Console.WriteLine("Digite um número:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num < 0)
+{
+    Console.WriteLine("O número é negativo");
+}
+else if(num == 0)
+{
+    Console.WriteLine("O número é zero");
+}
+else
+{
+    Console.WriteLine("O número é positivo");
+}
+
+Console.ReadLine();
