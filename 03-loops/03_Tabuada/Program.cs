56640d80a00afb78b5e5f2a7fe2347b9588c7a7d
pedromeireles23 <pemeirels@gmail.com>
2026-03-11 22:35:09 -0300

    loop 03

---
+++ 03-loops/03_Tabuada/Program.cs
@@ -0,0 +1,13 @@
+﻿// TODO 📝 Mostre a tabuada de um número.
+
+Console.WriteLine("Tabuada do 05");
+
+int numero = 5;
+
+for (int i = 1; i <= 10; i++)
+{
+    int tabuada = numero * i;
+    Console.WriteLine($"{numero} x {i} = {tabuada}");
+}
+
+Console.ReadLine();
