9b0efb3eed4d786fb7b44c9d8fa4c2b95438820a
pedromeireles23 <pemeirels@gmail.com>
2026-03-11 22:29:51 -0300

    loop 02

---
+++ 03-loops/02_SomaNumerosLoop/Program.cs
@@ -0,0 +1,13 @@
+﻿// TODO 📝 Some números de 1 a 5.
+
+int[] numeros = {1, 2, 3, 4, 5 };
+int somaNumeros = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    somaNumeros += numeros[i];   
+}
+
+Console.WriteLine(somaNumeros);
+
+Console.ReadLine();
