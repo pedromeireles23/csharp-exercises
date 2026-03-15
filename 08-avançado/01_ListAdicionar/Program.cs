8877dfe4edf9315fe981ccdb307d694843902fa1
pedromeireles23 <pemeirels@gmail.com>
2026-03-15 09:59:03 -0300

    listas 01,02,03

---
+++ 08-avançado/01_ListAdicionar/Program.cs
@@ -0,0 +1,23 @@
+﻿//TODO 📝 Crie uma List<int> e adicione 5 números.
+
+var numeros = new List<int> { 20,30,40,50,60};
+Console.WriteLine("Lista Original:");
+foreach (var numero in numeros)
+{
+    Console.WriteLine(numero);
+}
+
+//TODO 📝 Remova um número da lista.
+
+Console.WriteLine("Removendo 20");
+numeros.Remove(20);
+foreach (var numero in numeros)
+{
+    Console.WriteLine(numero);
+}
+
+//TODO 📝 Mostre quantos itens tem na lista.
+
+Console.WriteLine($"Quantidade de itens: {numeros.Count}");
+
+Console.ReadLine();
