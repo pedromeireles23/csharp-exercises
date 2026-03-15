7b7bf1284896ffc01880abe572ec0c9df0d81320
pedromeireles23 <pemeirels@gmail.com>
2026-03-14 21:44:59 -0300

    array 01

---
+++ 05-arrays/01_ArrayNumeros/Program.cs
@@ -0,0 +1,11 @@
+﻿//TODO 📝 Crie um array com 5 números e imprima.
+
+int[] arrNumbers = { 1, 2, 3, 4, 5};
+
+foreach (int number in arrNumbers)
+{
+    Console.WriteLine(number);
+}
+
+
+Console.ReadLine();
