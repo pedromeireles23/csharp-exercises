70b0cc13d16b0ebbf912eaf7148c6b9ef98394e1
pedromeireles23 <pemeirels@gmail.com>
2026-03-14 22:17:22 -0300

    array 02, 03, 04

---
+++ 05-arrays/02_ArrayOrdenar/Program.cs
@@ -0,0 +1,27 @@
+﻿//TODO 📝 Ordene um array.
+Console.WriteLine("Array sem modificações:");
+int[] arr = { 2, 8, 5, 20, 10 };
+foreach (int number0 in arr)
+{
+    Console.WriteLine(number0);
+}
+Console.WriteLine("Array ordenado:");
+Array.Sort(arr);
+foreach (int number in arr)
+{
+    Console.WriteLine(number);
+}
+
+// TODO 📝 Inverta um array.
+Console.WriteLine("Array invertido:");
+Array.Reverse(arr);
+
+foreach (int number2 in arr)
+{
+    Console.WriteLine(number2);
+
+}
+
+Console.WriteLine(Array.IndexOf(arr, 20 ));
+
+Console.ReadLine();
